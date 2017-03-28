9c7cfa84a402db739d7e6096ca72198e1fcd7db2
tvardy <tvardy@gmail.com>
2017-03-28 21:36:53 +0100

    feat(update-change-log): use `generate-changelog` package

--- src/create-commit/update-change-log.js
+++ src/create-commit/update-change-log.js
@@ -2,7 +2,7 @@
 var path = require('path');
 
 // 3rd party modules
-var changelog = require('conventional-changelog');
+var changelog = require('generate-changelog');
 var when = require('when');
 
 // modules
@@ -14,19 +14,45 @@ module.exports = updateChangeLog;
 // implementation
 function updateChangeLog(options) {
   return when.promise(function (resolve, reject) {
-    var writeStream = fs.createWriteStream(
-      path.resolve(options.directory, 'CHANGELOG.md')
-    );
+    var config = {};
+    var readFile = path.join(options.directory, 'CHANGELOG.md');
+    var writeFile = path.join(options.directory, '.CHANGELOG.md');
+    var writeStream = fs.createWriteStream(writeFile);
 
     writeStream.on('finish', onWriteEnd);
 
-    changelog({
-      path: options.directory,
-      preset: 'angular',
-      releaseCount: 0
-    }).pipe(writeStream);
+    config[options.type || 'patch'] = true;
+
+    changelog
+      .generate(config)
+      .then(writeChanges)
+      .then(readRest);
+
+    function writeChanges(chunk) {
+      writeStream.write(chunk);
+    }
+
+    function readRest() {
+      fs.createReadStream(readFile).pipe(writeStream);
+    }
 
     function onWriteEnd(err) {
+      if (err) {
+        reject(err);
+      } else {
+        fs.unlink(readFile, onUnlink);
+      }
+    }
+
+    function onUnlink(err) {
+      if (err) {
+        reject(err);
+      } else {
+        fs.rename(writeFile, readFile, finish);
+      }
+    }
+
+    function finish(err) {
       if (err) {
         reject(err);
       } else {
